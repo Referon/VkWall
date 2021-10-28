09219187191b05f23d65c2e5899b2e06fb5436f0
Arseny <senika-2010@mail.ru>
2021-10-28 12:03:08 +1000

    added collection methods

--- src/main/kotlin/WallService.kt
+++ src/main/kotlin/WallService.kt
@@ -1,5 +1,30 @@
 object WallService {
 
     private var posts = emptyArray<Post>()
+    private var id = 0
 
+    fun add(post: Post): Post {
+        posts += post
+
+        for ((id, post) in posts.withIndex()) {
+            if (post.id != this.id) {
+                posts[id] = post.copy(id = post.id + 1)
+            }
+        }
+        id++
+        return posts.component1()
+
+    }
+
+    fun update(post: Post): Boolean {
+        for ((id, post) in posts.withIndex()) {
+
+            if (post.id == this.id - 1) {
+                posts[id] = post.copy(id = this.id, date = post.date)
+                return true
+            }
+        }
+        return false
+    }
 }
+
